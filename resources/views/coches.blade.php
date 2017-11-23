30332ee401ab6c90351126af7ba6c14bf94b3024
“07asier” <asier2223@gmail.com>
2017-11-23 20:54:13 +0100

    Añadiendo como funciona

--- resources/views/coches.blade.php
+++ resources/views/coches.blade.php
@@ -1,6 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
+    <script type="text/javascript">
+
+        window.onload = init;
+
+        // asocia los eventos necesarios
+        function init() {
+            let formulario = document.getElementById("formulario");
+            formulario.addEventListener("submit", validar);
+            formulario.elements["problema"].addEventListener("keypress", longitudMax);
+        }
+
+        // previene escribir más caracteres de los permitidos en el campo "problema"
+        function longitudMax(event){
+            if (this.value.length === 100){
+                alert("El tamaño máximo del campo es 100.");
+                event.preventDefault();
+            }
+        }
+
+        // comprueba si el formulario está rellenado correctamente
+        function validar(event){
+            let formulario = document.getElementById("formulario");
+            for (let i=0;i<formulario.elements.length;i++){
+                let elemento = formulario.elements[i];
+                if(esIncorrecto(elemento)){
+                    elemento.className = "error";
+                    event.preventDefault();
+                }
+                else {
+                    elemento.className = "correcto";
+                }
+            }
+        }
+
+        // dice si el campo es incorrecto en funcion del tipo de campo
+        function esIncorrecto(elemento){
+            if(elemento.name === "numserie" || elemento.name === "marca" || elemento.name === "motor"){
+                return estaVacio(elemento);
+            }
+            else if (elemento.name === "modelo"){
+                return telefonoIncorrecto(elemento);
+            }
+            else if (elemento.name === "mensaje"){
+                return emailIncorrecto(elemento);
+            }
+        }
+
+        // comprueba si el campo está vacío
+        function estaVacio(campo){
+            if (campo.value.length === 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        // comprueba si el campo cumple los requisitos de un número de telefono
+        function telefonoIncorrecto(campo){
+            if (campo.value.length < 9 || isNaN(campo.value)){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        // comprueba si el campo cumple los requisitos de un email válido
+        function emailIncorrecto(campo){
+            if (  campo.value.indexOf("@") < 0 ||
+                (campo.value.indexOf("@") > campo.value.lastIndexOf(".") )
+            ){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+    </script>
+
+
 
 
     <div class="container">
@@ -13,7 +95,7 @@
         <div class="row well-sm well">
             <div class="col-md-8 "> <!-- col-md-8 -->
                 <div class="">
-                    <form>
+                    <form id="formulario">
                         <div class="row">
 
                             <div class="">
@@ -25,9 +107,6 @@
                                 </span>
                                         <input type="text" class="form-control" id="numserie" placeholder="" required="required" /></div>
                                 </div>
-
-
-
 
 
                                 <div class="form-group">
@@ -67,7 +146,7 @@
 
                             <div class="">
                                 <div class="form-group">
-                                    <label for="name">
+                                    <label for="mensaje">
                                         Mensaje</label>
                                     <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
                                               placeholder="Mensaje"></textarea>
--- resources/views/comofunciona.blade.php
+++ resources/views/comofunciona.blade.php
@@ -7,13 +7,16 @@
 </div>
 
 <div class="container">
+    <h3>Cómo funciona</h3>
     <div class="row well-sm well">
-        <h3>Cómo funciona</h3>
-        <ul>
-            <li></li>
-
-
-        </ul>
+        <h4><strong>· Paso 1</strong></h4>
+            <p>Una vez abierta la caja, encontraremos el dispositivo que tendremos que enchufar al coche</p>
+            <img src="images/obd.png"  width="250" height="150">
+        <h4><strong>· Paso 2</strong></h4>
+            <p>Abrimos enchufamos el OBD en la ranura del coche. La ranura se encuentra debajo del volante.Si no encuentras dónde está la ranura busca aqui:<a href="https://blog.reparacion-vehiculos.es/conector-obd-coche"> ¿Dónde está mi ranura?</a> </p>
+            <img src="images/obd2.jpg"  width="250" height="150">
+        <h4><strong>· Paso 3</strong></h4>
+            <p>Abrimos la aplicación web, insertamos nuestro coche en la seccion <a href="coches">coches</a> , introduciendo el numero de serie del OBD correspondiente, la marca del vehículo, el modelo etc ...</p>
     </div>
 </div>
 
